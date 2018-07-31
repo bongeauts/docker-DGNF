e34624a8052c66992969a165c6e75b53c04e446b
bongeauts <bergonzi.luca92@gmail.com>
2018-07-31 18:20:23 +0200

    pyside installing

---
+++ DGF/app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    url(r'^$', views.index, name='index' ),
+    # url(r'^boxes/$', views.BoxList.as_view(), name='boxes'),
+    # url(r'^boxes/cube/$', views.cube, name='cube'),
+    # url(r'^boxes/(?P<pk>\d+)/$', views.createCube, name='createCube'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- DGF/app/views.py
+++ DGF/app/views.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+# from .models import Box
+#from .serializers import BoxSerializer
+from django.template import loader
 # Create your views here.
+
+import sys
+sys.path.append('/usr/lib/freecad/lib')
+import FreeCAD
+import Part
+import importWebGL
+
+def index(request):
+    return HttpResponse("<h1>this is app1 homepage")
