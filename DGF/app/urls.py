7a92e2c4be3065274479cf803ce9b34b3d06599f
bongeauts <bergonzi.luca92@gmail.com>
2018-08-02 11:52:43 +0200

    added freecad lib

--- DGF/app/urls.py
+++ DGF/app/urls.py
@@ -4,6 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^$', views.index, name='index' ),
+    url(r'^box/$', views.createCube, name='createCube')
     # url(r'^boxes/$', views.BoxList.as_view(), name='boxes'),
     # url(r'^boxes/cube/$', views.cube, name='cube'),
     # url(r'^boxes/(?P<pk>\d+)/$', views.createCube, name='createCube'),
--- DGF/app/views.py
+++ DGF/app/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -13,7 +13,7 @@ from django.template import loader
 # Create your views here.
 
 import sys
-sys.path.append('/usr/lib/freecad/lib')
+#sys.path.append('/usr/lib/freecad/lib')
 import FreeCAD
 import Part
 import importWebGL
@@ -21,3 +21,18 @@ import importWebGL
 def index(request):
     version = FreeCAD.Version()
     return HttpResponse("<h1>this is app1 homepage " + str(version))
+
+def createCube(request):
+    doc = FreeCAD.newDocument("Unnamed1")
+    doc.addObject("Part::Box", "Box")
+    doc.ActiveObject.Label = "Cube"
+    doc.recompute()
+    FreeCAD.getDocument("Unnamed1").getObject("Box").Length = 100
+    FreeCAD.getDocument("Unnamed1").getObject("Box").Width = 200
+    FreeCAD.getDocument("Unnamed1").getObject("Box").Height = 100
+    doc.recompute()
+    __objs__ = []
+    __objs__.append(FreeCAD.getDocument("Unnamed1").getObject("Box"))
+    importWebGL.export(__objs__, u"/DGF/app/templates/cube1.html")
+
+    return render(request, u"/DGF/app/templates/cube1.html")
